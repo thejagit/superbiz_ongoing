6d87d999cb8e3355a82c1c4e6bec01455ff15b45
Thejani Kulathunga <thejani.kulathunga@gmail.com>
2026-01-13 00:08:52 +0530

    first commit

---
+++ config/urls.py
@@ -0,0 +1,42 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from apps.accounts.views import index
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('accounts/', include('accounts.urls')),
+    path('', index, name='index'),
+]
+
+if settings.DEBUG:
+    #import debug_toolbar  # type: ignore
+
+    # Use += to APPEND to the existing list, and NO "..." inside!
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+       # path("__debug__/", include(debug_toolbar.urls)),
+    ]
+
+# after Url patterns for media files,
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
